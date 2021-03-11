987426756b372bd307e9252d708f821aaf6f0e10
Edwin Kofler <edwin@kofler.dev>
2021-03-10 22:30:06 -0800

    feat: add dotty package

---
+++ packages/dotty/package.sh
@@ -0,0 +1,15 @@
+# shellcheck shell=bash
+
+name="dotty"
+version="v0.2.0"
+integrity_check="no"
+identity_check="no"
+
+pkg_install() {
+	# extract
+	bm_get "https://github.com/eankeen/dotty/releases/download/$version/dotty"
+	chmod +x "$name"
+
+	# final
+	bm_place_bin "$name" "$version"
+}
